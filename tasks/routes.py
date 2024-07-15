a869ea1324f430d45d1b41d56006ab99dcb328d1
Temirlan504 <113613569+Temirlan504@users.noreply.github.com>
2024-07-15 17:16:23 -0400

    Created templates
    Created templates for home, login, register

--- tasks/routes.py
+++ tasks/routes.py
@@ -3,4 +3,12 @@ from tasks import app
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('index.html', title='My App')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
